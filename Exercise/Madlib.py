a9d09b87f049feda68759332c810616474e4f346
melakhaae <darrennmelakha@gmail.comm>
2025-08-19 23:29:03 +0700

    Madlib exercise

---
+++ Exercise/Madlib.py
@@ -0,0 +1,6 @@
+noun1 = str(input("Enter a noun: "))
+noun2 = str(input("Enter a noun: "))
+
+print(f"Today I went to the zoo and saw a {noun1}.")
+print(f"When I saw the {noun1}, I was so excited!")
+print(f"Then it got hot so I bought a/an {noun2}.")
