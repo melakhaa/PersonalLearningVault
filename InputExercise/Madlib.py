9827b74d8980c26ea97e464b919e5480b9aa700a
melakhaae <darrennmelakha@gmail.comm>
2025-08-20 23:53:04 +0700

    Learning math

--- InputExercise/Madlib.py
+++ InputExercise/Madlib.py
@@ -1,5 +1,5 @@
-noun1 = str(input("Enter a noun: "))
-
+noun1 = str(input("Enter noun1: "))
+noun2 = str(input("Enter noun2: "))
 
 print(f"Today I went to the zoo and saw a {noun1}.")
 print(f"When I saw the {noun1}, I was so excited!")
--- main.py
+++ main.py
@@ -1,8 +1,1 @@
-# input
-name = str(input("Enter your name: "))
-age = int(input("Enter your age: "))
-
-age = age + 1
-
-print(f"Hello {name}")
-print(f"your age is {age}")
+# Conditional
