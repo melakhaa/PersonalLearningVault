ec348be6e2d84cbf047590c43a6ceea9dc7886f4
melakhaae <darrennmelakha@gmail.comm>
2025-08-21 00:05:27 +0700

    testing contributions

--- Math/main.py
+++ Math/main.py
@@ -22,12 +22,17 @@ import math
 
 
 # Exercise 
-radius = float(input("Enter the radius of the circle: "))
+# radius = float(input("Enter the radius of the circle: "))
 
-Circumference = 2 * math.pi * radius
-Area = math.pi * pow(radius, 2)
+# Circumference = 2 * math.pi * radius
+# Area = math.pi * pow(radius, 2)
+a = float(input("Enter the value of a: "))
+b = float(input("Enter the calue of b: "))
 
-print(f"The circumference of the circle is: {round(Circumference)}")
-print(f"The area of the circle is: {round(Area)}")
+Hypotenuse = math.sqrt(pow(a, 2) + pow(b, 2))
+
+# print(f"The circumference of the circle is: {round(Circumference)}")
+# print(f"The area of the circle is: {round(Area)}")
+print(f"The hypotenuse of the triangle is: {Hypotenuse}")
 
 #test contribuution 
