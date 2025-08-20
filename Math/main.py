a7c1e6a1e7769469424d79b4f51ab25db3090018
melakhaae <darrennmelakha@gmail.comm>
2025-08-20 23:57:00 +0700

    testing contributions

---
+++ Math/main.py
@@ -0,0 +1,33 @@
+import math
+
+# print(math.pi)
+
+# result = math.sqrt(16)
+# result = math.ceil(4.2)
+# result = math.floor(4.8)
+
+# print(result)
+
+# x = 3.14
+# y = 2
+# z = 5
+
+# result =  round(x)
+# result = abs(y)
+# result = pow(y, z) 
+# result = max(x, y, z)
+# result = min(x, y, z)
+
+# print(result)
+
+
+# Exercise 
+radius = float(input("Enter the radius of the circle: "))
+
+Circumference = 2 * math.pi * radius
+Area = math.pi * radius ** 2
+
+print(f"The circumference of the circle is: {round(Circumference)}")
+print(f"The area of the circle is: {round(Area)}")
+
+#test contribuution 
