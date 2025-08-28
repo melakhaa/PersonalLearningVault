1ef410e8f082048f5f40753f8ad93bca8d550644
melakhaa <darrennmelakha@gmail.com>
2025-08-29 00:30:18 +0700

    Conditional Excercise #3

--- conditional/exercise.py
+++ conditional/exercise.py
@@ -21,17 +21,33 @@
 
 # Python weight converter
 
-weight = float(input("Enter your weight: "))
-unit = input("(K)g or (L)bs: ")
+# weight = float(input("Enter your weight: "))
+# unit = input("(K)g or (L)bs: ")
 
-if unit == "K":
-    weight = weight * 2.205
-    unit = "Lbs"
-    print(f"You are {round(weight, 3)} {unit}.")
-elif unit == "L":
-    weight = weight / 2.205
-    unit = "Kg"
-    print(f"You are {round(weight, 3)} {unit}.")
+# if unit == "K":
+#     weight = weight * 2.205
+#     unit = "Lbs"
+#     print(f"You are {round(weight, 3)} {unit}.")
+# elif unit == "L":
+#     weight = weight / 2.205
+#     unit = "Kg"
+#     print(f"You are {round(weight, 3)} {unit}.")
+# else:
+#     print("Invalid unit.")
+
+# Python temperature converter
+
+unit = input("Convert from (C)elsius or (F)ahrenheit: ")
+temp = float(input("Enter the temperature: "))
+
+if unit == "C":
+    temp = round((temp * 9/5) + 32)
+    unit = "F"
+    print(f"The temperature is {round(temp, 3)} {unit}.")
+elif unit == "F":
+    temp = round((temp - 32) * 5/9)
+    unit = "C"
+    print(f"The temperature is {round(temp, 3)} {unit}.")
 else:
     print("Invalid unit.")
 
